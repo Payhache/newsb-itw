43133de2757c01285cfde8a77b8c623bb59ea2e1
Payhache <62991093+Payhache@users.noreply.github.com>
2022-11-29 19:26:39 +0100

    Update media.component.ts

--- src/app/media.component.ts
+++ src/app/media.component.ts
@@ -4,12 +4,9 @@ import { IMedia } from './model/IMedia';
 @Component({
   selector: 'app-media',
   template: `
-    <h1>
-     {{media.basename}}
-    </h1>
-    <p></p>
+    <h1> {{media.basename}} </h1>
     <p> Date du média : {{media.created_at | date: 'M/d/yy'}} </p>
-    <p *ngIf="media.type === 'video'">Durée : {{media.duration || "n/c" | number: '3.0-2'}} min</p>
+    <p *ngIf="media.type === 'video'"> Durée : {{media.duration || "n/c" | number: '3.0-2'}} min</p>
     <p *ngIf="media.type !== 'video'"> Type de média : {{media.type}}</p>
   `,
   styles: [`host: {margin-top: 2rem;}`
